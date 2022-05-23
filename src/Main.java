526678a373c47cd523effd9743c9361f8425f439
Anshuman Mohapatra <amohapatra2001@gmail.com>
2022-05-23 20:11:15 +0530

    delete and update done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Main {
+	static Scanner y = new Scanner(System.in);
 	static String username;
 	static String  password;
 	static String name;
@@ -11,7 +12,6 @@ public class Main {
 	static String address;
 	public static void main(String[] args){
 		System.out.println("******Welcome to Student Management App*****");
-		Scanner y = new Scanner(System.in);
 		System.out.print("Username : ");
 		username = y.nextLine();
 		System.out.print("Password : ");
@@ -27,43 +27,103 @@ public class Main {
 		}
 		
 		while(to_run) {
-			
+			System.out.println();
 			System.out.println("Press 1 to insert student detail");
+			System.out.println("Press 2 to delete student detail");
+			System.out.println("Press 3 to update student detail");
+			System.out.println("Press 4 to display student detail");
 			int choice = y.nextInt();
+			//add a student
 			if(choice==1) {
-				
-				System.out.println("Name : ");
-				y.nextLine();
-				name = y.nextLine();
-				System.out.print("Age : ");
-				age = y.nextInt();
-				y.nextLine();
-				System.out.print("Phone : ");
-				phone = y.nextLine();
-				System.out.print("Fathers Name : ");
-				fathers_name = y.nextLine();
-				System.out.print("DOB (eg. 21-MAR-09) : ");
-				dob = y.nextLine();
-				System.out.print("Gender : ");
-				gender = y.nextLine();
-				System.out.print("Address : ");
-				address = y.nextLine();
+				takeInfo();
 				Student s = new Student(name,age,phone,fathers_name,dob,gender,address);				
-//				s.setAge(age);				
-//				s.setPhone(phone);				
-//				s.setFathers_name(fathers_name);				
-//				s.setDob(dob);				
-//				s.setGender(gender);				
-//				s.setAddress(address);
 				boolean res = Operations.InsertStudent(s);
 				if(res) {
 					System.out.println("Student added successfully....");
 				}else {
 					System.out.println("Somthing went wrong , please try later");
 				}
+			}else if(choice==2) {
+				System.out.println("Press 1 to delete by id");
+				System.out.println("Press 2 to delete by phone number");
+				int ch = y.nextInt();
+				if(ch==1) {
+					System.out.println("Enter id : ");
+					int id = y.nextInt();
+					boolean res = Operations.DeleteStudentById(id);
+					if(res) {
+						System.out.println("Student deleted successfully....");
+					}else {
+						System.out.println("Somthing went wrong , please try later");
+					}
+				}else if(ch==2) {
+					System.out.println("Enter phone number : ");
+					y.nextLine();
+					phone  = y.nextLine();
+					boolean res = Operations.DeleteStudentByPhone(phone);
+					if(res) {
+						System.out.println("Student deleted successfully....");
+					}else {
+						System.out.println("Somthing went wrong , please try later");
+					}
+				}
+			}else if(choice==3) {
+				System.out.println("Press 1 to update by id");
+				System.out.println("Press 2 to update by phone number");
+				int ch = y.nextInt();
+				
+				if(ch==1) {
+					System.out.println("Enter id : ");
+					int id = y.nextInt();
+					takeInfo();
+					Student s = new Student(name,age,phone,fathers_name,dob,gender,address);
+					boolean res = Operations.UpdateStudentById(s,id);
+					if(res) {
+						System.out.println("Student updated successfully....");
+					}else {
+						System.out.println("Somthing went wrong , please try later");
+					}
+				}else if(ch==2) {
+					System.out.println("Enter phone number : ");
+					y.nextLine();
+					String phone_for_update = y.next();
+					takeInfo();
+					Student s = new Student(name,age,phone,fathers_name,dob,gender,address);
+					boolean res = Operations.UpdateStudentByPhone(s,phone_for_update);
+					if(res) {
+						System.out.println("Student updated successfully....");
+					}else {
+						System.out.println("Somthing went wrong , please try later");
+					}
+				}
+				
+			}else if(choice==4) {
+				
+			}else if(choice==5) {
+				break;
 			}
 		}
-		
+		System.out.println("Thank You !!");
+	}
+	
+	public static void takeInfo() {
+
+		System.out.println("Name : ");
+		y.nextLine();
+		name = y.nextLine();
+		System.out.print("Age : ");
+		age = y.nextInt();
+		y.nextLine();
+		System.out.print("Phone : ");
+		phone = y.nextLine();
+		System.out.print("Fathers Name : ");
+		fathers_name = y.nextLine();
+		System.out.print("DOB (eg. 21-MAR-09) : ");
+		dob = y.nextLine();
+		System.out.print("Gender : ");
+		gender = y.nextLine();
+		System.out.print("Address : ");
+		address = y.nextLine();
 	}
 
 }
--- src/Operations.java
+++ src/Operations.java
@@ -24,4 +24,78 @@ public class Operations {
 		}
 		return result;
 	}
+	
+    public static boolean DeleteStudentById(int id) {
+		
+		try {
+			Connection connection = Connector.createConnection();
+			String query = "delete from student where id=?";
+			PreparedStatement p = connection.prepareStatement(query);
+			p.setInt(1, id);
+			p.executeUpdate();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+    
+    public static boolean DeleteStudentByPhone(String phone) {
+		
+		try {
+			Connection connection = Connector.createConnection();
+			String query = "delete from student where phone=?";
+			PreparedStatement p = connection.prepareStatement(query);
+			p.setString(1,phone);
+			p.executeUpdate();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+    
+    public static boolean UpdateStudentById(Student stud,int id) {
+		
+		try {
+			Connection connection = Connector.createConnection();
+			String query = "update student set name=?,age=?,phone=?,fathers_name=?,dob=?,gender=?,address=? where id=?";
+			PreparedStatement p = connection.prepareStatement(query);
+			p.setString(1, stud.getName());
+			p.setInt(2, stud.getAge());
+			p.setString(3,stud.getPhone());
+			p.setString(4, stud.getFathers_name());
+			p.setString(5, stud.getDob());
+			p.setString(6,stud.getGender());
+			p.setString(7, stud.getAddress());
+			p.setInt(8, id);
+			p.executeUpdate();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+    
+    public static boolean UpdateStudentByPhone(Student stud,String phone_for_update) {
+		
+		try {
+			Connection connection = Connector.createConnection();
+			String query = "update student set name=?,age=?,phone=?,fathers_name=?,dob=?,gender=?,address=? where phone=?";
+			PreparedStatement p = connection.prepareStatement(query);
+			p.setString(1, stud.getName());
+			p.setInt(2, stud.getAge());
+			p.setString(3,stud.getPhone());
+			p.setString(4, stud.getFathers_name());
+			p.setString(5, stud.getDob());
+			p.setString(6,stud.getGender());
+			p.setString(7, stud.getAddress());
+			p.setString(8, phone_for_update);
+			p.executeUpdate();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
